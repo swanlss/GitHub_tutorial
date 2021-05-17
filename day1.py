ba4f516a97a3f037456f186ceff82c26a5046bb7
Maartje <brouwer.mm@gmail.com>
2021-05-17 10:59:46 +0200

    Update day1.py

--- day1.py
+++ day1.py
@@ -3,3 +3,24 @@
 # Date: 210517
 # 2 Day GitHub course, provided by Matthew Humphreys
 # =============================================================================
+
+# Git = command line tool to track changes.
+# Github desktop does the same thing, but click based, user friendly.
+
+# Github is the cloud where everything is stored
+
+
+# Repository = repo = a folder on your computer that is tracked by git
+
+# Better not use a dropbox or onedrive as local path.
+
+# Make changes
+# Commit to main = local repo
+
+# Revert a change:
+# Go to history, right click the change you want to revert and 
+# choose revert changes in commit
+
+# If you make different changes, 
+# you can commit them separately with separate comments
+
